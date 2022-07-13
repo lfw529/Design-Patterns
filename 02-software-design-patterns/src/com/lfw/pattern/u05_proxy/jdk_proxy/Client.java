b9368c68ae5c1c70b61ff51e433cd3cdb3427ea0
lfw529 <differentlee1012@163.com>
2022-07-13 11:04:52 +0800

    1111

---
+++ 02-software-design-patterns/src/com/lfw/pattern/u05_proxy/jdk_proxy/Client.java
@@ -0,0 +1,19 @@
+package com.lfw.pattern.u05_proxy.jdk_proxy;
+
+public class Client {
+    public static void main(String[] args) {
+        //获取代理对象
+        //1,创建代理工厂对象
+        ProxyFactory factory = new ProxyFactory();
+        //2,使用factory对象的方法获取代理对象
+        SellTickets proxyObject = factory.getProxyObject();
+        //3,调用卖调用的方法
+        proxyObject.sell();
+
+        System.out.println(proxyObject.getClass());
+
+        //让程序一直执行, 理解底层原理
+        while (true) {
+        }
+    }
+}
