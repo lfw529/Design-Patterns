c088b61802b0663eeeeee4128e990e53c9457b25
lfw529 <differentlee1012@163.com>
2022-07-19 11:06:34 +0800

    111

---
+++ 02-software-design-patterns/src/main/java/com/lfw/pattern/u13_strategy/jdk/demo.java
@@ -0,0 +1,17 @@
+package com.lfw.pattern.u13_strategy.jdk;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class demo {
+    public static void main(String[] args) {
+        Integer[] data = {12, 2, 3, 2, 4, 5, 1};
+        // 实现降序排序
+        Arrays.sort(data, new Comparator<Integer>() {
+            public int compare(Integer o1, Integer o2) {
+                return o2 - o1;
+            }
+        });
+        System.out.println(Arrays.toString(data)); //[12, 5, 4, 3, 2, 2, 1]
+    }
+}
